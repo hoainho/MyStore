c157e804e83af2c11facdd53cacda0c8a6b43d82
RemAlw <nhoxtvt@gmail.com>
2020-06-20 03:36:07 +0700

    product tab 50%

--- js/style.js
+++ js/style.js
@@ -2,7 +2,14 @@
 //AOS
 AOS.init({
   });
-
+  
+$('.product-tab--container_content__box').slick({
+    dots: false,
+    slidesToShow: 5,
+    slidesToScroll: 1,
+    touchMove: false
+});
+    
 //   $('#select-button').click(
 //     function(){ $('#select-box').addClass('is-zindex') },
 //     function(){ $('#select-box').removeClass('is-zindex') }
