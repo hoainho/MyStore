18b06efdbcfc59d1f70a8c573cdb1a081a1ed043
RemAlw <nhoxtvt@gmail.com>
2020-06-20 18:58:37 +0700

    Product tabs - 100%

--- js/style.js
+++ js/style.js
@@ -6,11 +6,26 @@ AOS.init({
 $('.product-tab--container_content__box').slick({
     dots: false,
     slidesToShow: 5,
-    slidesToScroll: 1,
-    touchMove: false
+    slidesToScroll: 3,
+    touchMove: true,
+    swipe:true,
+
 });
-    
-//   $('#select-button').click(
-//     function(){ $('#select-box').addClass('is-zindex') },
-//     function(){ $('#select-box').removeClass('is-zindex') }
-// )
+
+
+$(document).ready(function(){
+$('.product-tab--container_tittle__item').click(
+  function(){ 
+    $('.product-tab--container_tittle__item').removeClass('active-tab--item'),
+    $(this).addClass('active-tab--item')
+  });
+
+});
+$(document).ready(function(){
+  $('.product-tab--container_tittle__link').click(
+    function(){ 
+      $('.product-tab--container_tittle__link').removeClass('active-tab--link'),
+      $(this).addClass('active-tab--link')
+    });
+  });
+
