49667dc3b4e1fb06b827b30965cbf97d827af792
RemAlw <nhoxtvt@gmail.com>
2020-06-21 23:46:20 +0700

    add time speed for slick

--- js/style.js
+++ js/style.js
@@ -9,24 +9,88 @@ AOS.init({
 $(document).ready(function(){
   
   $('.product-tab--container_content__box').slick({
-    dots: false,
-    slidesToShow: 5,
-    slidesToScroll: 3,
-    touchMove: true,
-    swipe:true,
+      dots: false,
+      accessibility:true,
+      slidesToShow: 5,
+      slidesToScroll: 3,
+      autoplay:true,
+      autoplaySpeed: 100,
+      centerMode:true,  
+      initialSlide: 1,
+      lazyLoad:'ondemand',
+      pauseOnHover:true,
+      swipe:true,
+      touchMove:true,
+      useCSS:true,
+      useCSS3:true, 
+      useTransform:true,
+
+
+
+ 
+
+
+
 
 });
 $('.product-tab--container_content__category').slick({
-  dots: false,
-  slidesToShow: 4,
-  slidesToScroll: 3,
-  touchMove: true,
-  swipe:true,
+      dots: false,
+      accessibility:true,
+      slidesToShow: 5,
+      slidesToScroll: 3,
+      autoplay:true,
+      autoplaySpeed: 100,
+      centerMode:true,  
+      initialSlide: 3,
+      lazyLoad:'ondemand',
+      pauseOnHover:true,
+      swipe:true,
+      touchMove:true,
+      useCSS:true,
+      useCSS3:true, 
+      useTransform:true,
 
 });
+$('.product-tab--container_content__category-SC').slick({
+  dots: false,
+  accessibility:true,
+  slidesToShow: 5,
+  slidesToScroll: 33,
+  autoplay:true,
+  autoplaySpeed: 100,
+  centerMode:true,  
+  initialSlide: 2,
+  lazyLoad:'ondemand',
+  pauseOnHover:true,
+  swipe:true,
+  touchMove:true,
+  useCSS:true,
+  useCSS3:true, 
+  useTransform:true,
+
+});
+$('.product-tab--container_content__category-KB').slick({
+  dots: false,
+  accessibility:true,
+  slidesToShow: 5,
+  slidesToScroll: 33,
+  autoplay:true,
+  autoplaySpeed: 200,
+  centerMode:true,  
+  initialSlide: 1,
+  lazyLoad:'ondemand',
+  pauseOnHover:true,
+  swipe:true,
+  touchMove:true,
+  useCSS:true,
+  useCSS3:true, 
+  useTransform:true,
+
+});
+
   
   });
-  
+sly.init();
 
 $(document).ready(function(){
 $('.product-tab--container_tittle__item').click(
@@ -43,4 +107,5 @@ $(document).ready(function(){
       $(this).addClass('active-tab--link')
     });
   });
+
 
