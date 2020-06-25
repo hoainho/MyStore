2fec0f89e297673558d10ad2f89aed64ecfd03c0
RemAlw <nhoxtvt@gmail.com>
2020-06-25 23:08:11 +0700

    logo fixed

--- js/style.js
+++ js/style.js
@@ -5,9 +5,24 @@ AOS.init({
   });
 //LoadPage follow to CrollY
 window.onload = function(){
+  //Menu
   const ShowNavbar =document.querySelector(".navbar-content--menu");
   window.addEventListener('scroll',menuFixed);
+  //Logo box
+  const LGnavbar =document.querySelector(".navbar-content--header_main__features___logo ");
+  const LGsize = document.querySelector(".navbar-content--header_main__features___logo-box--logo");
+  window.addEventListener('scroll',logosize);
+  window.addEventListener('scroll',logofixed);
+  //Cart
+  const CartN = document.querySelectory(".navbar-content--header_main__features___control-cart--name");
+  const CartI = document.querySelectory(".navbar-content--header_main__features___control-cart--icon");
+  const CartBox =document.querySelector(".navbar-content--header_main__features___control-cart");
+  window.addEventListener('scroll',Cart);
+  window.addEventListener('scroll',CartName);
+  window.addEventListener('scroll',CartIcon);
 
+
+  
   function menuFixed(){
         if(window.scrollY >=200) {
             ShowNavbar.style.position= 'fixed';
@@ -23,7 +38,66 @@ window.onload = function(){
 
           }
     };
- 
+  function logofixed(){
+              if(window.scrollY >=200 ){
+                LGnavbar.style.position ='fixed';
+                LGnavbar.style.height ='18%';
+                LGnavbar.style.width ='13%';
+                LGnavbar.style.top = '-1.5rem';
+                LGnavbar.style.zIndex = '12';
+              }
+              else{
+                LGnavbar.style.position ='relative';
+                LGnavbar.style.bottom ='8%';
+                LGnavbar.style.width ='100%';
+                LGnavbar.style.height ='100%';
+
+              }
+      }
+  function logosize(){
+      if(window.scrollY >=200 ){
+        LGsize.style.fontSize = '4rem';
+      }
+      else{
+        LGsize.style.fontSize ='5rem';
+      }
+    }
+    function Cart(){
+      if(window.scrollY >=200 ){
+        CartBox.style.position ='fixed';
+        CartBox.style.right ='-4rem';
+        CartBox.style.top ='-2rem';
+        CartBox.style.width = '20%';
+        CartBox.style.height ='20%';
+        CartBox.style.zIndex ='12';
+      }
+      else{
+        CartBox.style.position ='relative';
+        CartBox.style.left ='3rem';
+        CartBox.style.top ='-2rem';
+        CartBox.style.height ='100%';
+        
+        
+      }
+    }
+    function CartName(){
+      if(window.scrollY >=200 ){
+        CartN.style.width = '31%';
+        CartN.style.height ='34%';
+      }
+      else{
+        CartN.style.width = '50%';
+        CartN.style.height ='100%';
+      }
+    }
+    function CartIcon(){
+      if(window.scrollY >=200 ){
+        CartI.style.margin = '0';
+      }
+      else{
+        CartI.style.margin = '0 1rem';
+      }
+    }
 }  
 
 
